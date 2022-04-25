0dd6a08dfbd2387eb09e5bd53fa06663a3433b09
Даниил Иванов <65729958+AliensWithdrawPeople@users.noreply.github.com>
2022-04-25 19:56:29 +0700

    Small changes.

--- radCor.cpp
+++ radCor.cpp
@@ -18,15 +18,11 @@ private:
     Float_t pRatio;
     Float_t psi;
     Float_t pK0;
+    double s;
     // Maybe: 1 / alpha = 137.071999.
     static const double alpha = 1 / 137.035999;  
     static const double kaonMass = 497.611;   
     static const double electronMass = 0.511;
- 
-
-    double kaonMass; 
-    double electronMass;
-    double s;
 
     // Born cross-section.
     double Sigma0(const double &s);
@@ -148,71 +144,7 @@ double RadCor::RadCorEval()
     return Krc.Integral(0., 1., 0., 1.) / N;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-double EnergyHandler::RadCor(TF1 *massFunc, Float_t E, Float_t pRatio, Float_t psi)
+int radCor()
 {
-    double kaonMass = 497.611; double electronMass = 0.511;
-    double s = 4 * E * E;
-    double beta_ = sqrt(1 - kaonMass * kaonMass / E / E);
-    TF1 Li2("Li2", "-TMath::Log(1-t)/t");
-    double L = 2 * TMath::Log(2*E / electronMass);
-    double a = TMath::Pi()*TMath::Pi() / 6 - 1/4;
-    double b = -1 + (1/beta_-1)*L/2 + 1/beta_*TMath::Log((1+beta_)/2) + 
-                (1/beta_ + beta_)/2 *(-Li2.Integral(0,(beta_-1)/(1+beta_)) + Li2.Integral(0, (1-beta_)/(1+beta_)) - TMath::Pi()*TMath::Pi()/12 + 
-                L*TMath::Log((1+beta_)/2)-2*L*TMath::Log(beta_) + 1.5*pow(TMath::Log((1+beta_)/2), 2) -0.5*pow(TMath::Log(beta_), 2) - 
-                3*TMath::Log(beta_)*TMath::Log((1+beta_)/2) + L + 2*TMath::Log((1+beta_)/2) );
-
-    /* 
-    D(x,s) = D_gamma + D_e+e-. 
-    [0] - b=2*alpha/pi * (L-1); [1] - L; [2] - 2*m_e/E.
-    For definition go to https://arxiv.org/abs/hep-ph/9703456v1.
-    */
-    TF1 D("D func", "pow([0]/2*(1-x), [0]/2-1) * ( 1+3*[0]/8+[0]*[0]/16*(9/8-TMath::Pi()*TMath::Pi()/3)) - [0]/4*(1+x)+ \
-        [0]*[0]/32*(4*(1+x)*TMath::Log(1/(1-x)) + (1+3*x*x)/(1-x)*TMath::Log(1/x)-5-x) + \
-        pow([0]/2*(1-x), [0]/2-1)*(-[0]*[0]/288*(2*[1] - 15)) + pow((1/137/TMath::Pi()), 2) * ( 1/12/(1-x) * \
-        pow((1-x-[2]), [0]/2)*(pow(pow(TMath::Log((1-x), 2) /[2]/[2])-5/3), 2) *(1+x^2+[0]/6*(pow(TMath::Log((1-x), 2) /[2]/[2])-5/3)) +\
-        [1]*[1]/4*(2/3*(1-x^3)/x+1/2*(1-x) + (1+x)*TMath::Log(x)) ) * ((1-x-[2] > 0) ? 1 : 0) ");
-
-    /*
-    Cross-section of e+e- -> KsKl.
-    For definition go to https://arxiv.org/abs/1604.02981v3 or https://doi.org/10.1142/S0217751X92001423.
-    TO-DO!!!
-    */
-    TF1 sigma0("sigma0", "1");
-
-    // Krc(s, x1, x2); 
-    // For definition go to https://arxiv.org/abs/hep-ph/9703456v1.
-    TF2 Krc("K RadCor", [&](double* x, double*p) { 
-        massFunc->SetParameters(E * (1-x[0]) * (1-x[1]), pRatio);
-        double bForDFormula = 2/137/TMath::Pi()*(L - 1);
-        D.SetParameters(bForDFormula, L, 2 * electronMass / E);
-        //sigma0.SetParameters(); // TO-DO!!!
-        return (fabs(p[0] - 1) < 0.1 ? massFunc->Eval(psi) : 1) * (1 + 2/137/TMath::Pi() * (1+a+b)) * D.Eval(x[0]) * D.Eval(x[1]) * sigma0(s*(1-x[0]) * (1-x[1])); 
-        }, 0, 1, 0, 1, 1);
-    Krc.SetParameter(0, 0);
-    Double_t N = Krc.Integral(0., 1., 0., 1.);
     
-    Krc.SetParameter(0, 1);
-    return Krc.Integral(0., 1., 0., 1.) / N;
 }
