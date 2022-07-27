b679ac4c2cce211f643b4c595a697bffe89dcd2c
Даниил Иванов <65729958+AliensWithdrawPeople@users.noreply.github.com>
2022-07-27 16:05:05 +0700

    RC still doesn't give right answer.

---
+++ auxFunc.cpp
@@ -0,0 +1,48 @@
+#include "TF1.h"
+#include "TGraphErrors.h"
+
+int auxFunc()
+{
+    double pRatio = 0.99999;
+    double psi = 2.56547;
+
+    double energy = 514;
+    double mass = 499.161;
+    double massK = 497.614;
+
+
+    auto massFunc = new TF1("MassLnY", "sqrt([0] * [0] * (1 - (1 + sqrt(1 - [1] *[1]) * cos(x))*(1 - sqrt(1 - [1] * [1] * (1 - 4 * 139.57 * 139.57 / [0] / [0])))/ [1] / [1] ))");
+    massFunc->SetParameter(1, (1 - pRatio*pRatio) / (1 + pRatio*pRatio));
+
+    //auto tmpFunc = new TF1("MassLnY", 
+    //"sqrt(x * x * (1 - (1 + sqrt(1 - [1] *[1]) * cos([0]))*(1 - sqrt(1 - [1] * [1] * (1 - 4 * 139.57 * 139.57 / x / x)))/ [0] / [0] ))", 480, 520);
+    auto tmpFunc = new TF1("MassLnY", "sqrt(x * x * (1 - (1 + sqrt(1 - [1] *[1]) * cos([0]))*(1 - sqrt(1 - [1] * [1] * (1 - 4 * 139.57 * 139.57 / x / x)))/ [1] / [1] ))");
+    tmpFunc->SetParameters(psi, (1 - pRatio*pRatio) / (1 + pRatio*pRatio));
+    massFunc->SetParameter(0, energy);
+    double Emin = tmpFunc->GetX(490, 450, 550, 1e-5, 10000);
+    double Emax = tmpFunc->GetX(505, 450, 550, 1e-5, 10000);
+    std::cout<< Emin << " : " << Emax << std::endl;
+
+
+    auto revMassFunc = new TF1("revMassFunc", "2*TMath::ACos(TMath::Sqrt((1 - x * x / [0] / [0])/(1-4*139.57 * 139.57 / [0] / [0])))");
+    revMassFunc->SetParameter(0, energy);
+    std::cout << "At E = " << energy << " and M = " << mass << " Psi = " << revMassFunc->Eval(mass) << std::endl;
+
+
+    std::vector<Float_t> vM = {497.645, 497.64, 497.648, 497.633, 497.676, 497.742};
+    std::vector<Float_t> vMerr = {0.0049, 0.0053, 0.0055, 0.0047, 0.0072, 0.0148};
+    std::vector<Float_t> vE = {505, 508, 509, 510, 511, 514};
+    std::vector<Float_t> zeroes(vM.size(), 0.0);
+    TGraphErrors gr(vM.size(), vE.data(), vM.data(), zeroes.data(), vMerr.data());
+    //gr.DrawClone();
+
+    std::vector<Float_t> vPsi = {2.73532, 2.73491, 2.73476, 2.73462, 2.73463};
+    std::vector<Float_t> vPsiErr = {4.07562e-04, 3.35361e-04, 2.88769e-04, 2.57586e-04, 2.35667e-04};
+    std::vector<Float_t> vWidth = {0.1, 0.15, 0.2, 0.25, 0.3};
+    std::vector<Float_t> zeroes2(vPsi.size(), 0.0);
+    TGraphErrors gr2(vPsi.size(), vWidth.data(), vPsi.data(), zeroes2.data(), vPsiErr.data());
+
+    //gr2.DrawClone();
+
+    return 0;
+}
--- rc.py
+++ rc.py
@@ -3,13 +3,25 @@ import numpy as np
 from scipy import integrate
 from scipy.interpolate import make_interp_spline
 from scipy.misc import derivative
+from scipy.optimize import root_scalar, RootResults
 
-pi:float = np.pi
-alpha:float = 1 / 137.035999
-mK:float = 497.614  
+pi: float = np.pi
+alpha: float = 1 / 137.035999
+mK: float = 497.614  
 """Kaon mass (MeV)"""
-me:float = 0.511 
+me: float = 0.511 
 """Electron mass (MeV)"""
+mPi: float = 139.570
+"""Charged pion mass (Mev)"""
+
+def massFunc(s: float, psi: float)->float:
+    eta: float = (1 - 0.9999*0.9999) / (1 + 0.9999*0.9999)
+    # pions' beta ^2
+    b: float = 1 - 4 * mPi**2 / (s / 4)
+    return (s / 4 * (1 - (1 + (1 - eta**2)**0.5 * np.cos(psi)) * (1 - (1 - eta**2 * b)**0.5 )/ eta / eta ))**0.5
+
+def massNC(s: float, psi: float, sigmaPsi: float)->float:
+    return massFunc(s, psi) - sigmaPsi**2 / 2 * derivative(lambda x: massFunc(s, x), psi, 1e-5, 2, order=9)
 
 def FormFactor(s: float)->complex:
     MPhi = 1.01919e+03
@@ -24,26 +36,28 @@ def FormFactor(s: float)->complex:
     par9 = -3.89597e-03
     #double Mks = 497.614;
 
-    Maphi1680:float = 1680
-    Mrho2150:float = 2150
-    Momega1420:float = 1425
-    Momega1650:float = 1670
-    WPhi1680:float = 170.
-    Womega1420:float = 400.
-    Womega1650:float = par6
-    Warho2150:float = 500.
-    aphi:complex = MPhi*MPhi/complex(s-MPhi*MPhi,np.sqrt(s) * WPhi)
-    aphi1680:complex = Maphi1680*Maphi1680/complex(s-Maphi1680*Maphi1680,np.sqrt(s)*WPhi1680)
-    aomega1420:complex = Momega1420*Momega1420/complex(s-Momega1420*Momega1420, -np.sqrt(s)*Womega1420)
-    aomega1650:complex = Momega1650*Momega1650/complex(Momega1650*Momega1650-s, -np.sqrt(s)*Womega1650)
-    arho2150:complex = Mrho2150*Mrho2150/complex(Mrho2150*Mrho2150-s, -np.sqrt(s)*Warho2150)
-    ATot:complex = Smax*aphi + complex(par3,par4) + par5*aomega1420 + par7*aomega1650 + par8*aphi1680 - par9*arho2150
+    Maphi1680: float = 1680
+    Mrho2150: float = 2150
+    Momega1420: float = 1425
+    Momega1650: float = 1670
+    WPhi1680: float = 170.
+    Womega1420: float = 400.
+    Womega1650: float = par6
+    Warho2150: float = 500.
+    aphi: complex = MPhi*MPhi/complex(s-MPhi*MPhi,np.sqrt(s) * WPhi)
+    aphi1680: complex = Maphi1680*Maphi1680/complex(s-Maphi1680*Maphi1680,np.sqrt(s)*WPhi1680)
+    aomega1420: complex = Momega1420*Momega1420/complex(s-Momega1420*Momega1420, -np.sqrt(s)*Womega1420)
+    aomega1650: complex = Momega1650*Momega1650/complex(Momega1650*Momega1650-s, -np.sqrt(s)*Womega1650)
+    arho2150: complex = Mrho2150*Mrho2150/complex(Mrho2150*Mrho2150-s, -np.sqrt(s)*Warho2150)
+    ATot: complex = Smax*aphi + complex(par3,par4) + par5*aomega1420 + par7*aomega1650 + par8*aphi1680 - par9*arho2150
     return np.sqrt(1 / alpha)*ATot
 
 def SigmaBorn(s:float)->float:
-    energies:List = [1004.066, 1010.466, 1012.955, 1015.068, 1016.105, 1017.155, 1017.156, 1018.046, 1019.118,  1019.214, 1019.421, 1019.902, 
+    # Old version (incorrect)
+    #return 204.5 * alpha * alpha * pow(1 - 4 * mK * mK / s, 3./2.) * 2 / 3 * pi / s * pow(abs(FormFactor(s)), 2.)
+    energies: List = [1004.066, 1010.466, 1012.955, 1015.068, 1016.105, 1017.155, 1017.156, 1018.046, 1019.118,  1019.214, 1019.421, 1019.902, 
                     1021.222, 1021.309, 1022.078,  1022.744, 1023.264,  1025.320, 1027.956, 1029.090, 1033.907, 1040.028,  1049.864,  1050.862,  1059.947]
-    cross_sections:List = [6.87, 42.16, 96.74, 219.53, 366.33, 628.15, 624.76, 996.62, 1413.65,  1433.05, 1434.84, 1341.91,  833.20, 
+    cross_sections: List = [6.87, 42.16, 96.74, 219.53, 366.33, 628.15, 624.76, 996.62, 1413.65,  1433.05, 1434.84, 1341.91,  833.20, 
                             807.54, 582.93,  443.71, 377.77,  199.26, 115.93, 96.96, 50.12, 31.27,  16.93,  17.47,  12.09]
     data = np.array([energies, cross_sections]).T 
     data2 = data[data[:,0].argsort()]
@@ -52,53 +66,59 @@ def SigmaBorn(s:float)->float:
     y = spline(x)
     y = np.where(x<=2*mK, 0, y)
     return np.interp(s, (x**2), y)
-    # Old version (incorrect)
-    #return 204.5 * alpha * alpha * pow(1 - 4 * mK * mK / s, 3./2.) * 2 / 3 * pi / s * pow(abs(FormFactor(s)), 2.)
 
 def F(x: float, s: float)->float:
+    """ F is radiator (see Fadin-Kuraev and Achasov's presentation)
+    Args:
+        x (float): variable of integration in integral for corrected cross-section; For x << 1 x is energy carried away by photons and pairs.
+        s (float): Mandelstam variable, s = E_cm ^ 2
+
+    Returns:
+        float: value of F at x for certain energy. 
+    """
     # L = ln(s / m_e^2)
-    L:float = np.log(s / me / me)
+    L: float = np.log(s / me / me)
     # beta = 2*alpha/pi * (L-1)
-    beta:float = 2 * alpha / pi *(L - 1)
+    beta: float = 2 * alpha / pi *(L - 1)
 
-    p1:float = beta * x**(beta - 1.) * (1 + alpha / pi * (pi**2 / 3 - 1./2.) + 3./4. * beta - 1./24. * beta**2 * (L / 3. + 2 * pi ** 2 - 37./4.) )
-    p2:float = beta * (1. - x /2.)
+    p1: float = beta * x**(beta - 1.) * (1 + alpha / pi * (pi**2 / 3 - 1./2.) + 3./4. * beta - 1./24. * beta**2 * (L / 3. + 2 * pi ** 2 - 37./4.) )
+    p2: float = beta * (1. - x /2.)
 
-    p3:float = 4. * (2. - x) * np.log(1. / x)
-    p4:float = 1. / x * (1. + 3. * (1.-x)**2) * np.log(1./(1.-x))
+    p3: float = 4. * (2. - x) * np.log(1. / x)
+    p4: float = 1. / x * (1. + 3. * (1.-x)**2) * np.log(1./(1.-x))
 
     # Photon part
-    phPart:float = p1  - p2 + beta * beta / 8. * (p3 + p4 - 6. + x)
+    phPart: float = p1  - p2 + beta * beta / 8. * (p3 + p4 - 6. + x)
 
-    E:float = np.sqrt(s / 4.)
+    E: float = np.sqrt(s / 4.)
 
     # Real e+e- part.
     if(x - 2 * me / E > 0):
-        p5:float = 1 / 6. / x * (x - 2 * me / E)**beta
-        p6:float = (np.log(s * x * x / me / me) - 5./3.)**2.
-        p7:float = beta/3.*(np.log(s * x**2 / (me**2)) - 5./3)
-        p8:float = L*L/2. * (2./3.*(1-(1-x)**3) / (1.-x) - (2.-x)*np.log(1./(1.-x)) + x/2.)
+        p5: float = 1 / 6. / x * (x - 2 * me / E)**beta
+        p6: float = (np.log(s * x * x / me / me) - 5./3.)**2.
+        p7: float = beta/3.*(np.log(s * x**2 / (me**2)) - 5./3)
+        p8: float = L*L/2. * (2./3.*(1-(1-x)**3) / (1.-x) - (2.-x)*np.log(1./(1.-x)) + x/2.)
 
-        elPart:float = (alpha / pi)**2 * (p5 * p6 * (2 - 2*x + x*x + p7) + p8)
+        elPart: float = (alpha / pi)**2 * (p5 * p6 * (2 - 2*x + x*x + p7) + p8)
     else:
-        elPart:float = 0
+        elPart: float = 0
 
     return phPart + elPart
+
+def epsCalc(s: float, massUpperLimit: float)->float:
+    res: RootResults = root_scalar(lambda psi: massFunc(s, psi) - massUpperLimit, xtol=1e-5, x0=2, x1=3)
+    print("Root finding is", str(res.converged) + "; number of iteration =", res.function_calls)
+    psiCr: float = res.root
+    enLim: float = (mK**2 - 4 * (mPi * np.cos(psiCr / 2))**2)**0.5 / np.sin(psiCr / 2)
+    if res.converged:
+        print("Energy limit =", enLim)
+    # (2*enLim - 2 * mK) / mK
+    return 1 - 4 * enLim**2 / s if res.converged else 1
 
 def SigmaCorrected(s:float, eps=1)->float:
     return integrate.quad(lambda x: SigmaBorn(s * (1-x)) * F(x, s), 0, eps, epsabs = 1e-6, epsrel=1e-4, limit=500)[0]
 
-def massFunc(s: float, psi: float)->float:
-    eta:float = (1 - 0.9999*0.9999) / (1 + 0.9999*0.9999)
-    # beta piona ^2
-    b:float = 1 - 4 * 139.57 * 139.57 / (s / 4)
-    return (s / 4 * (1 - (1 + (1 - eta**2)**0.5 * np.cos(psi)) * (1 - (1 - eta**2 * b)**0.5 )/ eta / eta ))**0.5
-    #return s**0.5 / 2 *(1 - (1 - 4 * 139.57 * 139.57 / (s / 4) ) * np.cos(psi / 2) * np.cos(psi / 2))**0.5
-
-def massNc(s: float, psi: float, sigmaPsi: float)->float:
-    return massFunc(s, psi) - sigmaPsi**2 / 2 * derivative(lambda x: massFunc(s, x), psi, 1e-5, 2, order=9)
-
-def GetMassCorrected(s: float, psi: float, sigmaPsi: float, Emin: float, Emax: float)->float:
+def GetMassCorrected(s: float, psi: float, sigmaPsi: float, eps: float)->float:
     """ Compute mass of Ks with radiative and nonlinearity correction.
 
     Args:
@@ -109,22 +129,16 @@ def GetMassCorrected(s: float, psi: float, sigmaPsi: float, Emin: float, Emax: f
     Returns:
         float: corrected mass of Ks
     """        
-    eps:float = (2*Emax - 2 * mK) / mK
-    conv:float = integrate.quad(lambda x: massNc(s * (1-x), psi, sigmaPsi) * SigmaBorn(s * (1-x)) * F(x, s), 0, 1, epsabs = 1e-6, epsrel=1e-4, limit=500)[0]
-    return conv / SigmaCorrected(s, 1)
+    conv: float = integrate.quad(lambda x: massNC(s * (1-x), psi, sigmaPsi) * SigmaBorn(s * (1-x)) * F(x, s), 0, eps, epsabs = 1e-6, epsrel=1e-4, limit=500)[0]
+    return conv / SigmaCorrected(s, eps)
 
-energies:float = [1004.066, 1010.466, 1012.955, 1015.068, 1016.105, 1017.155, 1017.156, 1018.046, 1019.118,  1019.214, 1019.421, 1019.902, 
-                    1021.222, 1021.309, 1022.078,  1022.744, 1023.264,  1025.320, 1027.956, 1029.090, 1033.907, 1040.028,  1049.864,  1050.862,  1059.947]
-sList:List = [x**2 for x in energies]
-#print([SigmaCorrected(foo) / SigmaBorn(foo) for foo in sList])
 
-energy:float =  2 * 511
-s:float = energy**2
-psi:float = 2.5997
-sigmaPsi:float = 1.64407e-02
-Emin:float = 506.4
-Emax:float = 522.577
+energy: float =  2 * 511
+s: float = energy**2
+psi: float = 2.5988
+sigmaPsi: float = 0.0164407
+massUpperLimit: float = 505
 
-print("Corrected Mass = ", GetMassCorrected(s, psi, sigmaPsi, Emin, Emax) )
+eps: float = epsCalc(s, massUpperLimit)
+print("Corrected Mass = ", GetMassCorrected(s, psi, sigmaPsi, eps), "eps =", eps)
 print("Mass = ", massFunc(s, psi) )
-print(SigmaBorn(4 * 497.5**2))
