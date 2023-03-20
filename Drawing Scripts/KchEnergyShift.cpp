28e760057a3d5b04e50a72dbbd04484f8d804204
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2023-03-21 02:22:12 +0700

    New Kch MC data processing & results.

--- Drawing Scripts/KchEnergyShift.cpp
+++ Drawing Scripts/KchEnergyShift.cpp
@@ -11,14 +11,16 @@ int KchEnergyShift()
 {
     vector<double> zeroes(100, 0.0);
 
-    vector<double> E = {505, 508, 508.5, 508.5, 508.5, 509, 509.5, 509.5, 509.5, 510, 510, 510, 510.5, 510.5, 511, 511, 514};
-    vector<double> Pavg = {106.339, 119.779, 121.882, 121.882, 121.882, 123.952, 125.989, 125.989, 125.989, 127.996, 127.996, 127.996, 129.974, 129.974, 131.924, 131.924, 143.10};
-    vector<double> energyShift = {5.567, 4.228, 4.084, 4.06, 4.086, 3.907, 3.754, 3.746, 3.778, 3.722, 3.711, 3.729, 3.684, 3.681, 3.559, 3.561, 3.106};
-    vector<double> energyShiftErr = {0.031, 0.005, 0.025, 0.021, 0.048,  0.010, 0.009, 0.026, 0.009, 0.009, 0.013, 0.009, 0.013, 0.008, 0.022, 0.009, 0.011};
-    vector<double> energyShiftMC = {5.443, 4.144,  3.922, 3.922, 3.922, 3.770, 3.673, 3.673, 3.673, 3.597, 3.597, 3.597, 3.487, 3.487, 3.300, 3.300, 2.860};
+vector<double> E = {505, 508, 508.5, 508.5, 508.5, 509, 509.5, 509.5, 509.5, 510, 510, 510, 510.5, 510.5, 511, 511, 514};
+vector<double> Pavg = {106.339, 119.779, 121.882, 121.882, 121.882, 123.952, 125.989, 125.989, 125.989, 127.996, 127.996, 127.996, 129.974, 129.974, 131.924, 131.924, 143.10};
+vector<double> energyShift = {5.567, 4.228, 4.084, 4.06, 4.086, 3.907, 3.754, 3.746, 3.778, 3.722, 3.711, 3.729, 3.684, 3.681, 3.559, 3.561, 3.106};
+vector<double> energyShiftErr = {0.031, 0.005, 0.025, 0.021, 0.048,  0.010, 0.009, 0.026, 0.009, 0.009, 0.013, 0.009, 0.013, 0.008, 0.022, 0.009, 0.011};
+vector<double> energyShiftMC_NoFSR = {5.443, 4.144,  3.922, 3.922, 3.922, 3.770, 3.673, 3.673, 3.673, 3.597, 3.597, 3.597, 3.487, 3.487, 3.300, 3.300, 2.860};
+vector<double> energyShiftMC_FSR = {5.462, 4.122, 3.913, 3.913, 3.913, 3.757, 3.670, 3.670, 3.670, 3.605, 3.605, 3.605, 3.484, 3.484, 3.314, 3.314, 2.879};
 
     TGraphErrors grShiftExp(Pavg.size(), Pavg.data(), energyShift.data(), zeroes.data(), energyShiftErr.data());
-    TGraphErrors grShiftMC(Pavg.size(), Pavg.data(), energyShiftMC.data(), zeroes.data(), zeroes.data());
+    TGraphErrors grShiftMC_NoFSR(Pavg.size(), Pavg.data(), energyShiftMC_NoFSR.data(), zeroes.data(), zeroes.data());
+    TGraphErrors grShiftMC_FSR(Pavg.size(), Pavg.data(), energyShiftMC_FSR.data(), zeroes.data(), zeroes.data());
 
     std::string fromStr = 
         "0.3890836 * pow(1+(493.677/x)*(493.677/x), 1.343142/2) + 0.5472272 + 0.3864033 * 0.3864033 *" 
@@ -32,10 +34,10 @@ int KchEnergyShift()
     grShiftExp.GetXaxis()->SetTitle("P_{avg}, #frac{MeV}{c}");
     // grShiftExp.GetXaxis()->SetTitle("E_{mean}, MeV");
     grShiftExp.GetYaxis()->SetTitle("#DeltaE, MeV");
-    grShiftMC.SetLineColor(kBlue);
+    grShiftMC_FSR.SetLineColor(kBlue);
 
     grShiftExp.DrawClone("AP");
-    grShiftMC.DrawClone("same");
+    grShiftMC_FSR.DrawClone("same");
     func.DrawClone("same");
 
     return 0;
--- energyStability.cpp
+++ energyStability.cpp
@@ -4,16 +4,28 @@ int energyStability()
 {
     gROOT->Reset();
     auto start = std::chrono::system_clock::now();
+    const std::vector<double> meanEnergies = {504.8, 507.862, 508.404, 508.957, 509.528, 509.956, 510.458, 511.035, 513.864};
+    const std::vector<std::string> energyPoints = {"505", "508", "508.5", "509", "509.5", "510", "510.5", "511", "514"};
 
     // auto enHandler = new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/expKpKm/kchExp508.5.root", 508.397, 0.008, 15, 4.06);
     // auto enHandler = new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/expKpKm/kchExp509.5.root", 509.529, 0.004, 15, 3.753);
-    auto enHandler = new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/expKpKm/kchExp510.root", 509.957, 0.005, 15, 3.6);
+    // auto enHandler = new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/expKpKm/kchExp510.root", 509.957, 0.005, 15, 3.6);
     // auto enHandler = new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/exкpKpKm/kchExp510.5.root", 510.454, 0.007, 15, 3.677);
     // auto enHandler = new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/expKpKm/kchExp511.root", 511.035, 0.009, 30, 0);
+    // auto enHandler = new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/MC/Kch/MC505_FSR.root", 504.8, 0.007, 15, 0);
 
-    // auto enHandler = new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/MC/Kch/MC_kch510.5.root", 510.454, 0.007, 15, 0);
-    
-    enHandler->DrawGraph();
+    std::vector<Energy*> handlers;
+    for(int i = 0; i < energyPoints.size(); i++)
+    { handlers.push_back(new Energy("C://work/Science/BINP/Kaon Mass Measure/tr_ph/MC/Kch/MC" + energyPoints[i] + "_FSR.root", meanEnergies[i], 0.007, 30, 0)); }
+
+    std::cout << "{";
+    for(auto &handler : handlers)
+    {
+        auto shift = handler->GetEnergyDiff();
+        for (auto k : shift)
+        { std::cout << -k.second << ", "; }
+    }
+    std::cout << "}" << std::endl;
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> diff = end - start; 
     std::cout << "exec time = " << diff.count() << std::endl;
