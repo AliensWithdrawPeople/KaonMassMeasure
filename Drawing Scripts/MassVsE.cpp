1489e621db57c8445f3b8b4d11193db103bf13fa
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2023-04-04 15:14:58 +0700

    New mass results.

--- Drawing Scripts/MassVsE.cpp
+++ Drawing Scripts/MassVsE.cpp
@@ -18,9 +18,11 @@ int MassVsE()
 
     std::vector<Float_t> vPavg = {157, 182, 207, 231, 235, 243, 254, 260};
 
-    std::vector<Float_t> vM_Exp = {497.541, 497.564, 497.536, 497.563, 497.549, 497.578, 497.589, 497.571, 497.601};
-    std::vector<Float_t> vM_Exp2 = {497.569, 497.579, 497.541, 497.574, 497.566, 497.601, 497.591, 497.571, 497.529};
-    std::vector<Float_t> vMerrExp = {0.010, 0.012, 0.013, 0.013, 0.011, 0.012, 0.013, 0.014, 0.024};
+    // std::vector<Float_t> vM_Exp = {497.541, 497.564, 497.536, 497.563, 497.549, 497.578, 497.589, 497.571, 497.601};
+    std::vector<Float_t> vM_Exp = {497.525, 497.561, 497.54, 497.553, 497.548, 497.573, 497.586, 497.565, 497.609};
+
+    std::vector<Float_t> vM_Exp2 = {497.557, 497.563, 497.534, 497.56, 497.547, 497.572, 497.596, 497.567, 497.6139};
+    std::vector<Float_t> vMerrExp = {0.036, 0.013, 0.009, 0.01, 0.004, 0.009, 0.008, 0.01, 0.028};
     std::vector<Float_t> vE = {505, 508, 508.5, 509, 509.5, 510, 510.5, 511, 514};
     TGraphErrors grRCNC_exp(vM_Exp.size(), vE.data(), vM_Exp.data(), zeroes.data(), vMerrExp.data());
     TGraphErrors grRCNC_exp2(vM_Exp.size(), vE.data(), vM_Exp2.data(), zeroes.data(), vMerrExp.data());
@@ -29,10 +31,14 @@ int MassVsE()
     grRCNC_exp.GetXaxis()->SetTitle("E_{beam}, MeV");
     grRCNC_exp.GetYaxis()->SetTitle("M^{(FullRec)}_{RC NC RecCor}, #frac{MeV}{c^{2}}");
     grRCNC_exp.SetName("EnControl");
+    grRCNC_exp2.SetName("NoEnControl");
 
     grRCNC_exp.DrawClone("AP");
     grRCNC_exp2.SetMarkerColor(kBlue);
-    // grRCNC_exp2.DrawClone("P Same");
+    grRCNC_exp2.SetLineColor(kBlue);
+    grRCNC_exp2.SetMarkerStyle(22);
+    grRCNC_exp2.SetMarkerSize(1.5);
+    grRCNC_exp2.DrawClone("P Same");
 
     return 0;
 }
