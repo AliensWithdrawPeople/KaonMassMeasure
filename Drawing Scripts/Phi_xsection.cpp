4e8f5b936c088807c07598bb04a39666d4e9da11
AliensWithdrawPeople <daniilivanov1606@gmail.com>
2023-07-05 16:45:50 +0700

    New phi to Kn xsection.

--- Drawing Scripts/Phi_xsection.cpp
+++ Drawing Scripts/Phi_xsection.cpp
@@ -9,20 +9,24 @@ int Phi_xsection()
 {
     std::vector<double> zeroes(100, 0.0);
 
-    // std::vector<double> energy = {500.9293, 502.9799, 504.8, 507.862, 508.404, 508.957, 509.528, 509.956, 510.458, 511.035, 511.4479, 513.864};
-    // std::vector<double> energy = {1001.859, 1005.96, 1009.6, 1015.724, 1016.808, 1017.914, 1019.056, 1019.912, 1020.916, 1022.07, 1022.896, 1027.728};
-    // std::vector<double> energySpread = {0.3492, 0.3625, 0.3626, 0.3937, 0.3473, 0.3556, 0.3819, 0.4158, 0.4071, 0.3791, 0.3914, 0.3672};
-
     std::vector<double> energy = {1.001859, 1.00596, 1.0096, 1.015724, 1.016808, 1.017914, 1.019056, 1.019912, 1.020916, 1.02207, 1.022896, 1.027728};
     std::vector<double> energySpread = {0.0003492, 0.0003625, 0.0003626, 0.0003937, 0.0003473, 0.0003556, 0.0003819, 0.0004158, 0.0004071, 0.0003791, 0.0003914, 0.0003672};
 
-    // std::vector<double> xsec_vis = {0.568, 1.6317, 3.0119, 37.9855, 68.9547, 121.7992, 180.229, 178.9924, 146.8741, 90.1184, 70.3458, 27.91};
-    // std::vector<double> xsec_vis_err = {0.033, 0.036, 0.082, 0.273, 0.237, 0.393, 0.305, 0.345, 0.424, 0.369, 0.378, 0.238};
+    // std::vector<double> energy = {1.015724, 1.016808, 1.017914, 1.019056, 1.019912, 1.020916};
+    // std::vector<double> energySpread = {0.0003937, 0.0003473, 0.0003556, 0.0003819, 0.0004158, 0.0004071};
 
-    std::vector<double> xsec_vis = {5.6382, 11.0821, 17.8353, 200.678, 360.1538, 625.5156, 908.8247, 900.9489, 734.6828, 444.511, 347.8403, 135.666};
-    std::vector<double> xsec_vis_err = {0.328, 0.248, 0.483, 1.441, 1.237, 2.017, 1.54, 1.735, 2.123, 1.821, 1.87, 1.156};
 
-    std::vector<double> efficiency = {0.100742, 0.14724, 0.168875, 0.189286, 0.191459, 0.194718, 0.19831, 0.198671, 0.199915, 0.202736, 0.202236, 0.205726};
+    // std::vector<double> xsec_vis = {219.29452, 379.46311, 661.0057, 991.93678, 987.60974, 783.37357};
+    // std::vector<double> xsec_vis_err = {1.33853, 1.12915, 2.10807, 2.17229, 2.4543, 2.47224};
+
+
+    std::vector<double> xsec_vis = {2.4649, 8.38149, 22.0801, 219.29452, 379.46311, 661.0057, 991.93678, 987.60974, 783.37357, 522.4324, 402.48297, 149.54976};
+    std::vector<double> xsec_vis_err = {0.14137, 0.16368, 0.53163, 1.33853, 1.12915, 2.10807, 2.17229, 2.4543, 2.47224, 2.01235, 1.94623, 1.14083};
+
+
+    std::vector<double> efficiency = {0.26591, 0.2577, 0.2517, 0.24325, 0.24028, 0.23805, 0.23718, 0.23539, 0.23547, 0.23485, 0.23438, 0.22889};
+    // std::vector<double> efficiency = {0.25902, 0.25123, 0.24398, 0.24325, 0.24028, 0.23805, 0.23718, 0.23539, 0.23547, 0.23485, 0.23438, 0.22889};
+    std::vector<double> efficiency_err = {0.000438, 0.000434, 0.000429, 0.000429, 0.000427, 0.000426, 0.000425, 0.000424, 0.000424, 0.000424, 0.000424, 0.00042};
     TFile *top = new TFile("vcs.root", "recreate");
 
     auto grVisXsec = new TGraphErrors(energy.size(), energy.data(), xsec_vis.data(), energySpread.data(), xsec_vis_err.data());
@@ -37,13 +41,15 @@ int Phi_xsection()
     top->Write();
     top->Save();
 
-    TGraphErrors grEff(energy.size(), energy.data(), efficiency.data(), zeroes.data(), zeroes.data());
-    grEff.GetYaxis()->SetTitle("#eps");
-    grEff.GetXaxis()->SetTitle("E_{avg}, MeV");
+    TGraphErrors grEff(energy.size(), energy.data(), efficiency.data(), zeroes.data(), efficiency_err.data());
+    grEff.GetYaxis()->SetTitle("#varepsilon_{MC}");
+    grEff.GetXaxis()->SetTitle("E_{cms}, GeV");
     grEff.SetName("eff");
     grEff.SetMarkerColor(kBlue);
     grEff.SetMarkerSize(1.5);
-    // grEff.DrawClone("AP");
+    grEff.DrawClone("AP");
 
     return 0;
+
+    // [p0]/([p4]*(x-[p5])*(x-[p5]))/(1-[p1]*exp([p2]*(x-[p3])))
 }
--- PhiMesonFit/PhiMesonFit.py
+++ PhiMesonFit/PhiMesonFit.py
@@ -1,32 +1,41 @@
 import numpy as np
-import uproot as up
 from xsection import Xsection
-import matplotlib.pyplot as ppl
-import matplotlib
-matplotlib.use('TkAgg')
+import uproot as up
+import scipy
 
-
-points = ["501", "503", "505", "508", "508.5", "509", "509.5", "510", "510.5", "511", "511.5", "514"]
-e_MC = np.array([0.100742, 0.14724, 0.168875, 0.189286, 0.191459, 0.194718, 0.19831, 0.198671, 0.199915, 0.202736, 0.202236, 0.205726])
-B_Kpipi = 0.692
-efficiency = e_MC * B_Kpipi
+E = np.array([501, 503, 505, 508, 508.5, 509, 509.5, 510, 510.5, 511, 511.5, 514])
+energy_points = ["501", "503", "505", "508", "508.5", "509", "509.5", "510", "510.5", "511", "511.5", "514"]
+energy = np.array([500.9293, 502.9799, 504.8, 507.862, 508.404, 508.957, 509.528, 509.956, 510.458, 511.035, 511.4479, 513.864])
 lumi = np.array([519.365, 1232.43, 454.856, 548.972, 1395.09, 979.506, 2621.62, 2041.31, 1053.46, 780.307, 561.27, 520.924])
 lumi_err = np.array([0.996744, 1.54306, 0.94111, 1.0394, 1.6559, 1.38908, 2.27821, 2.01353, 1.44786, 1.24862, 1.06109, 1.02696])
-mean_energies = [500.9293, 502.9799, 504.8, 507.862, 508.404, 508.957, 509.528, 509.956, 510.458, 511.035, 511.4479, 513.864]
-energy_info = dict(zip(points, mean_energies))
 
-N_events = []
-for energy_point in points:
-    with up.open(f"C://work/Science/BINP/Kaon Mass Measure/tr_ph/expKsKl/KsKl_Phi/exp{energy_point}.root:ksTree") as ksTree:
-        tree = ksTree.arrays(['emeas', 'demeas', 'runnum', 'ksdpsi', 'Y'], library="np")
-    N_events.append(len(tree['emeas']))
-N_events = np.array(N_events)
+lumi = np.array([463.81, 1213.94, 310.388, 512.997, 1364.79, 962.61, 2512.28, 1963.85, 1039.19, 697.626, 509.562, 505.252])
+lumi_err = np.array([1.05125, 1.70613, 0.866127, 1.12028, 1.8298, 1.53774, 2.48812, 2.20256, 1.60331, 1.31385, 1.125, 1.12422])
 
-xsec = np.round(N_events / efficiency / lumi, 5)
+                
+e_MC = np.array([0.26591, 0.2577, 0.2517, 0.24325, 0.24028, 0.23805, 0.23718, 0.23539, 0.23547, 0.23485, 0.23438, 0.22889])
+# e_MC = np.array([0.25902, 0.25123, 0.24398, 0.24325, 0.24028, 0.23805, 0.23718, 0.23539, 0.23547, 0.23485, 0.23438, 0.22889])
+e_MC_err = np.sqrt(e_MC * (1 - e_MC) * 1e-6)
+n_events = [304, 2622, 1725, 27365, 124438, 151469, 591058, 456543, 191690, 85594, 48069, 17295]
 
-vis_xsection = Xsection(np.array(mean_energies), xsec, (500, 515))
+# n_events = []
+# for point in energy_points:
+#     with up.open(f"C:/work/Science/BINP/Kaon Mass Measure/tr_ph/PhiXSection/Kn{point}.root:Kn") as KnTree: # type: ignore
+#         n_events.append(len(KnTree['emeas'].array())) # type: ignore
+        
+efficiency = e_MC
 
-a, b = vis_xsection.Get_Data()
+xsec_vis = n_events / lumi
+xsec_vis_err = np.sqrt( (np.sqrt(n_events) / lumi / efficiency)**2 + (n_events / efficiency / (lumi**2))**2 * lumi_err**2  * (n_events / lumi / (efficiency**2))**2 * e_MC_err**2 )
+xsec = n_events / lumi / efficiency
 
-ppl.plot(a, b)
-ppl.show()
+print("E_beam =", energy_points)
+print("N_events =", n_events)
+print("eff =", list(np.round(efficiency, 6)))
+print("eff_err =", list(np.round(e_MC_err, 6)))
+print("sigma_vis =", list(np.round(n_events / lumi, 5)))
+print("sigma_vis (efficiency accounted)=", list(np.round(xsec, 5)))
+print("sigma^(err)_vis =", list(np.round( xsec_vis_err, 5)) )
+
+# vis_xsection = Xsection(np.array(E), xsec, (500, 515))
+# a, b = vis_xsection.Get_Data()
